9e7eb5042c593bddfc751d07a3fc9c0e631f62a7
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-05-01 23:16:47 -0700

    added stuff

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,6 @@
 public class SkillDemo {
 
+    public static String getName() {
+        return "Pranay";
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void testGetName() {
+        assertEquals("Pranav", SkillDemo.getName());
+    }
+
+}
